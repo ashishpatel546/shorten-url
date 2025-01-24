66475d079b5bae60e0906e41d4367978de299bf3
Ashish Patel <ashi.patel546@gmail.com>
2025-01-24 19:09:08 +0530

    hot fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,6 @@ import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './shared/shared.module';
 import { RedisModule } from './redis/redis.module';
 import { ShortenUrlModule } from './url-shortner/url-shorten.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { User } from './user/entities/user.entity';
-import { RequestLimiterMiddleware } from './middlewares/request-limiter.middleware';
-import { MiddlewaresModule } from './middlewares/middlewares.module';
 
 @Module({
   imports: [
@@ -20,30 +15,11 @@ import { MiddlewaresModule } from './middlewares/middlewares.module';
       ignoreEnvFile: false,
       cache: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User],
-      synchronize: true,
-    }),
     SharedModule,
     ShortenUrlModule,
     RedisModule,
-    UserModule,
-    MiddlewaresModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(RequestLimiterMiddleware)
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
--- src/user/dto/update-plan.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Plan } from '../entities/plan.enum';
-import { IsEnum } from 'class-validator';
-
-export class UpdatePlanDto {
-  @IsEnum(Plan)
-  plan: Plan;
-}
